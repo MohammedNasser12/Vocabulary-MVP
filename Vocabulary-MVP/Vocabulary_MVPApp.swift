26c189392cadebf98ba927323942c3eaf2969585
Mohammed Nasser <mohamednasser827@yahoo.com>
2026-08-05 17:13:08 +0300

    Initial setup

---
+++ Vocabulary-MVP/Vocabulary_MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vocabulary_MVPApp.swift
+//  Vocabulary-MVP
+//
+//  Created by Mohamed Abd ElNasser on 05/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct Vocabulary_MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
